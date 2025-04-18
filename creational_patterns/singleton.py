8eb8c03e0eed65802aee9d7e1618a5674c07ac1a
SibusisoLebatla <slebatla1968@gmail.com>
2025-04-18 21:27:22 +0200

    Create singleton.py

---
+++ creational_patterns/singleton.py
@@ -0,0 +1,8 @@
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls)
+            cls._instance.connection_string = "Connected to DB"
+        return cls._instance
