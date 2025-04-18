87f4166d25b7477b3af750f9f4be5af106d1a2a7
SibusisoLebatla <slebatla1968@gmail.com>
2025-04-18 21:16:39 +0200

    user.py

--- src/user.py
+++ src/user.py
@@ -1,1 +1,17 @@
+class User:
+    def __init__(self, user_id, name, email, password):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.borrowed_books = []
+
+    def register(self):
+        print(f"{self.name} registered successfully.")
+
+    def login(self):
+        print(f"{self.name} logged in.")
+
+    def delete(self):
+        print(f"{self.name} account deleted.")
 
